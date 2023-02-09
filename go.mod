5b0259a88e13556df930bb15c0219b2bc99b878c
Bastian Bührig <bastian.buehrig@2bconsult.eu>
2023-02-09 14:56:38 +0100

    changes to github.com/atotto/clipboard and some linter-cleanups

--- go.mod
+++ go.mod
@@ -3,17 +3,13 @@ module github.com/wunderkind2k1/emojiterm
 go 1.19
 
 require (
+	github.com/atotto/clipboard v0.1.4
 	github.com/kyokomi/emoji/v2 v2.2.11
 	github.com/urfave/cli/v2 v2.24.2
-	golang.design/x/clipboard v0.6.3
 )
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
-	golang.org/x/mobile v0.0.0-20210716004757-34ab1303b554 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 )
--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
 	"log"
 	"os"
 
+	"github.com/atotto/clipboard"
 	"github.com/kyokomi/emoji/v2"
 	"github.com/urfave/cli/v2"
-
-	"golang.design/x/clipboard"
 )
 
 func main() {
 	useClipboard := false
 
 	app := &cli.App{
-		Name:    "emojiterm - text to emoji converter - Use underscore instead of space in emoji names",
-		Usage:   "emojiterm \"<space separeted list of tokens>\" | Example: emojiterm \"sun wine_glass beer)\"\nNames are based on this file: https://raw.githubusercontent.com/iamcal/emoji-data/master/emoji.json",
+		Name: "emojiterm - text to emoji converter - Use underscore instead of space in emoji names",
+		Usage: "emojiterm \"<space separeted list of tokens>\" | Example: emojiterm \"sun wine_glass beer)\"\n" +
+			"Names are based on this file: https://raw.githubusercontent.com/iamcal/emoji-data/master/emoji.json",
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:               "clipboard",
@@ -25,6 +25,7 @@ func main() {
 				DisableDefaultText: true,
 				Action: func(cCtx *cli.Context, b bool) error {
 					useClipboard = b
+
 					return nil
 				},
 			},
@@ -34,8 +35,8 @@ func main() {
 			fmt.Println(s)
 
 			if useClipboard {
-				if err := writeToClipboard(s); err != nil {
-					fmt.Println("Clipboard not supported")
+				if err := clipboard.WriteAll(s); err != nil {
+					fmt.Printf("Clipboard not supported: %v\n", err)
 				}
 			}
 
@@ -49,21 +50,15 @@ func main() {
 }
 
 func printEmojiArgsAsString(args []string) string {
-	s := ""
+	emojisAsString := ""
+
 	for index, arg := range args {
 		if index == 0 {
-			s = s + emoji.Sprintf(":%s:", arg)
+			emojisAsString += emoji.Sprintf(":%s:", arg)
 		} else {
-			s = s + emoji.Sprintf(" :%s:", arg)
+			emojisAsString += emoji.Sprintf(" :%s:", arg)
 		}
 	}
-	return s
-}
 
-func writeToClipboard(s string) error {
-	if err := clipboard.Init(); err != nil {
-		return err
-	}
-	clipboard.Write(clipboard.FmtText, []byte(s))
-	return nil
+	return emojisAsString
 }
