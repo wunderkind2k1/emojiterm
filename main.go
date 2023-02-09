f86e7e92cd3d2a1d3e0d501ee2b4170fae8f7e4a
Bastian Bührig <bastian.buehrig@2bconsult.eu>
2023-02-09 11:01:40 +0100

    adds the -c flag to write generated emojis to OS-clipboard

--- main.go
+++ main.go
@@ -7,15 +7,37 @@ import (
 
 	"github.com/kyokomi/emoji/v2"
 	"github.com/urfave/cli/v2"
+
+	"golang.design/x/clipboard"
 )
 
 func main() {
+	useClipboard := false
+
 	app := &cli.App{
 		Name:  "emojiterm - text to emoji converter - Use underscore instead of space in emoji names",
 		Usage: "emojiterm \"<space separeted list of tokens>\" | Example: emojiterm \"sun wine_glass beer)\"\nNames are based on this file: https://raw.githubusercontent.com/iamcal/emoji-data/master/emoji.json",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:               "c",
+				Usage:              "write emojis to clipbaord",
+				DisableDefaultText: true,
+				Action: func(cCtx *cli.Context, b bool) error {
+					useClipboard = b
+					return nil
+				},
+			},
+		},
 		Action: func(cCtx *cli.Context) error {
 			s := printEmojiArgsAsString(cCtx.Args().Slice())
 			fmt.Println(s)
+
+			if useClipboard {
+				if err := writeToClipboard(s); err != nil {
+					fmt.Println("Clipboard not supported")
+				}
+			}
+
 			return nil
 		},
 	}
@@ -35,4 +57,12 @@ func printEmojiArgsAsString(args []string) string {
 		}
 	}
 	return s
+}
+
+func writeToClipboard(s string) error {
+	if err := clipboard.Init(); err != nil {
+		return err
+	}
+	clipboard.Write(clipboard.FmtText, []byte(s))
+	return nil
 }
